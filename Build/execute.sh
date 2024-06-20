3fa69afdfad7fc3ae8c6db91a8fbb0f346c584db
Vijay Kumar <66196388+vijay2181@users.noreply.github.com>
2024-06-20 11:10:17 +0530

    Create execute.sh

---
+++ Build/execute.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export PATH="$PATH:/usr/local/bin"
+export USERID=$(id -u)
+export GROUPID=$(id -g)
+
+#Navigate to the script directory
+cd $(dirname $0)
+
+#Set environment variables
+CONTAINER_NAME="vijay-$(echo ${JOB_NAME} | tr '/ ' '._').${BRANCH_NAME}"
+[ -n "$CHANGE_ID" ] && CONTAINER_NAME="${CONTAINER_NAME}-PR${CHANGE_ID}"
+CONTAINER_NAME="${CONTAINER_NAME}-${BUILD_ID}"
+
+#Initialize Packer to install required plugins
+docker-compose -f build.yml run \
+      --rm -w "$WORKSPACE" \
+      --name "${CONTAINER_NAME}" \
+      packer init jenkins-packer-ami.pkr.hcl
+
+#Run the Packer build
+docker-compose -f build.yml run \
+      --rm -w "$WORKSPACE" \
+      --name "${CONTAINER_NAME}" \
+      packer build -var "region=${REGION}" \
+      -var "ami_name=${AMI_NAME}" \
+      -var "description=${DESCRIPTION}" jenkins-packer-ami.pkr.hcl
