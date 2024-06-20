107dae9e4aa082f604755404e5598a01fd39a130
Vijay Kumar <66196388+vijay2181@users.noreply.github.com>
2024-06-20 12:53:24 +0530

    Update execute.sh

--- Build/execute.sh
+++ Build/execute.sh
@@ -3,24 +3,28 @@ export PATH="$PATH:/usr/local/bin"
 export USERID=$(id -u)
 export GROUPID=$(id -g)
 
-#Navigate to the script directory
+# Navigate to the script directory
 cd $(dirname $0)
 
-#Set environment variables
+echo "REGION=${REGION}"
+echo "AMI_NAME=${AMI_NAME}"
+echo "DESCRIPTION=${DESCRIPTION}"
+
+# Set environment variables
 CONTAINER_NAME="vijay-$(echo ${JOB_NAME} | tr '/ ' '._').${BRANCH_NAME}"
 [ -n "$CHANGE_ID" ] && CONTAINER_NAME="${CONTAINER_NAME}-PR${CHANGE_ID}"
 CONTAINER_NAME="${CONTAINER_NAME}-${BUILD_ID}"
 
-#Initialize Packer to install required plugins
+# Initialize Packer to install required plugins
 docker-compose -f execute.yml run \
       --rm -w "$WORKSPACE" \
       --name "${CONTAINER_NAME}" \
-      packer init jenkins-packer-ami.pkr.hcl
+      packer init ../jenkins-packer-ami.pkr.hcl
 
-#Run the Packer build
+# Run the Packer build
 docker-compose -f execute.yml run \
       --rm -w "$WORKSPACE" \
       --name "${CONTAINER_NAME}" \
       packer build -var "region=${REGION}" \
       -var "ami_name=${AMI_NAME}" \
-      -var "description=${DESCRIPTION}" jenkins-packer-ami.pkr.hcl
+      -var "description=${DESCRIPTION}" ../jenkins-packer-ami.pkr.hcl
