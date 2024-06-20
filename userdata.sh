7947e57cedf7da4ea1c0ff3f9105ed94f6779adc
Vijay Kumar <66196388+vijay2181@users.noreply.github.com>
2024-06-20 18:34:27 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,5 @@
 yum update -y
 sudo yum install -y httpd
 sudo echo "<html><body><h1>Hello Vijay</h1></body></html>" | sudo tee /var/www/html/index.html
+sudo systemctl enable httpd
 sudo systemctl start httpd
