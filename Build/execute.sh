af6516d4c24ffce64cb586d144cc3362bbc585b8
Vijay Kumar <66196388+vijay2181@users.noreply.github.com>
2024-06-21 16:21:29 +0530

    Update execute.sh

--- Build/execute.sh
+++ Build/execute.sh
@@ -17,16 +17,14 @@ CONTAINER_NAME="vijay-$(echo "${JOB_NAME}" | tr '/ ' '._').${BRANCH_NAME}"
 [ -n "${CHANGE_ID}" ] && CONTAINER_NAME="${CONTAINER_NAME}-PR${CHANGE_ID}"
 CONTAINER_NAME="${CONTAINER_NAME}-${BUILD_ID}"
 
-# Initialize Packer to install required plugins
-docker-compose -f execute.yml run \
-      --rm -w "${WORKSPACE}" \
-      --name "${CONTAINER_NAME}" \
-      packer init "${WORKSPACE}"/jenkins-packer-ami.pkr.hcl
-
 # Run the Packer build
-docker-compose -f execute.yml run \
-      --rm -w "${WORKSPACE}" \
-      --name "${CONTAINER_NAME}" \
-      packer build -var "region=${REGION}" \
-      -var "ami_name=${AMI_NAME}" \
-      -var "description=${DESCRIPTION}" "${WORKSPACE}"/jenkins-packer-ami.pkr.hcl
+docker-compose -f builder.yml run \
+    --rm \
+    -w "$WORKSPACE" \
+    --name "${CONTAINER_NAME}" \
+    slave build \
+        -var "region=${REGION}" \
+        -var "ami_name=${AMI_NAME}" \
+        -var "description=${DESCRIPTION}" \
+        "${WORKSPACE}"/jenkins-packer-ami.pkr.hcl
+
